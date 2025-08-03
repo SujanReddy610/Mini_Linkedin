3265b85be23b25638a42e7c3ce89475cf2dc83f0
SujanReddy610 <139474884+SujanReddy610@users.noreply.github.com>
2025-08-03 17:41:59 +0530

    userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const { registerUser, loginUser, getUserProfile } = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
-
 const router = express.Router();
-
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/:id').get(protect, getUserProfile);
-
 module.exports = router;
