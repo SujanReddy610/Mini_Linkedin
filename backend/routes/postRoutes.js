8ec599ba2f237dbfd2fe829ac063bd62b4d088ac
SujanReddy610 <139474884+SujanReddy610@users.noreply.github.com>
2025-08-03 17:41:33 +0530

    postRoutes.js

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const { createPost, getPosts } = require('../controllers/postController');
 const { protect } = require('../middleware/authMiddleware');
-
 const router = express.Router();
-
 router.route('/').post(protect, createPost).get(protect, getPosts);
-
 module.exports = router;
