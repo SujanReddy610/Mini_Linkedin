f75f87af9fdffdd8f27d1ed820e54db749e83ca4
SujanReddy610 <139474884+SujanReddy610@users.noreply.github.com>
2025-08-03 17:43:24 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,12 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
 const postRoutes = require('./routes/postRoutes'); // <-- Add this line
-
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json()); // Body parser
-
-// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
@@ -23,10 +18,6 @@ mongoose.connect(process.env.MONGO_URI)
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
-
-// API Routes
 app.use('/api/users', userRoutes);
-app.use('/api/posts', postRoutes); // <-- Add this line
-
-// Start the server
+app.use('/api/posts', postRoutes); 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
